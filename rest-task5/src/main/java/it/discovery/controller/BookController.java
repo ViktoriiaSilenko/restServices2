277f353a687411e6a4bfc3fdc613e9731d02a586
ViktoriiaSilenko <victory.silenko@gmail.com>
2017-08-19 16:20:10 +0300

    @RequestMapping. CRUD for Book

--- rest-task5/src/main/java/it/discovery/controller/BookController.java
+++ rest-task5/src/main/java/it/discovery/controller/BookController.java
@@ -1,29 +1,56 @@
 package it.discovery.controller;
 
 import it.discovery.model.Book;
+import it.discovery.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/book")
 public class BookController {
 
-    @RequestMapping(value = "/newBook", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE, MediaType.APPLICATION_XML_VALUE})
+    private static int id = 1;
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @RequestMapping(value = "/save", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE, MediaType.APPLICATION_XML_VALUE},
+        method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.ACCEPTED)
     public Book getSampleBook() {
 
         Book book = new Book();
-        book.setId(1);
+        book.setId(id);
         book.setName("Test name");
         book.setYear(2017);
         book.setAuthor("Test author");
 
+        bookRepository.save(book);
+
+        id++;
+
         return book;
+    }
+
+    @RequestMapping(value = "/get/{id}", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public Book getById(@PathVariable("id") int id) {
+        return bookRepository.findById(id);
+    }
+
+    @RequestMapping(value = "/get", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public List<Book> getById() {
+        return bookRepository.findAll();
+    }
+
+    @RequestMapping(value = "/delete/{id}", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE, MediaType.APPLICATION_XML_VALUE},
+            method = RequestMethod.DELETE)
+    public boolean deleteById(@PathVariable("id") int id) {
+        return bookRepository.delete(id);
     }
 
 
